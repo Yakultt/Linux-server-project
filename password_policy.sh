1db839a1485083534610e79f37256af4ff059d74
Harit Sookmeewiriya <harit@Harits-MacBook-Pro.local>
2025-10-19 21:07:58 -0400

    password policy script

---
+++ password_policy.sh
@@ -0,0 +1,12 @@
+#Enforcing Strong Password Policy
+#Making sure users follow a strong password policy is necessary for system security. 
+#This script will configure the system’s password policy to enforce strong passwords.
+
+#!/bin/bash
+# Install the libpam-pwquality package to configure password policies
+sudo apt-get install libpam-pwquality -y
+# Set password complexity requirements
+sudo sed -i 's/# minlen = 8/minlen = 12/' /etc/security/pwquality.conf
+sudo sed -i 's/# minclass = 1/minclass = 4/' /etc/security/pwquality.conf
+# Enable password policy in PAM configuration
+sudo echo "password requisite pam_pwquality.so retry=3" >> /etc/pam.d/common-password
